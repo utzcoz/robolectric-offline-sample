2a492f842f514bd29ac562a5c89b592f14162c53
utzcoz <utzcoz@outlook.com>
2023-11-11 16:49:35 +0800

    Use compose compiler can work with Kotlin 1.9.20
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     }
     kotlinOptions { jvmTarget = "1.8" }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 
     testOptions {
