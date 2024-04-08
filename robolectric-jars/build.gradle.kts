74beb9b0351d652c447cb138ab3910463c49da58
utzcoz <43091780+utzcoz@users.noreply.github.com>
2024-04-08 18:37:50 +0800

    Bump preinstrumented jar to v6

--- robolectric-jars/build.gradle.kts
+++ robolectric-jars/build.gradle.kts
@@ -3,21 +3,21 @@ plugins {
 }
 
 val versions = listOf(
-    "14-robolectric-10818077-i4",
-    "13-robolectric-9030017-i4",
-    "12.1-robolectric-8229987-i4",
-    "12-robolectric-7732740-i4",
-    "11-robolectric-6757853-i4",
-    "10-robolectric-5803371-i4",
-    "9-robolectric-4913185-2-i4",
-    "8.1.0-robolectric-4611349-i4",
-    "8.0.0_r4-robolectric-r1-i4",
-    "7.1.0_r7-robolectric-r1-i4",
-    "7.0.0_r1-robolectric-r1-i4",
-    "6.0.1_r3-robolectric-r1-i4",
-    "5.1.1_r9-robolectric-r2-i4",
-    "5.0.2_r3-robolectric-r0-i4",
-    "4.4_r1-robolectric-r2-i4"
+    "14-robolectric-10818077-i6",
+    "13-robolectric-9030017-i6",
+    "12.1-robolectric-8229987-i6",
+    "12-robolectric-7732740-i6",
+    "11-robolectric-6757853-i6",
+    "10-robolectric-5803371-i6",
+    "9-robolectric-4913185-2-i6",
+    "8.1.0-robolectric-4611349-i6",
+    "8.0.0_r4-robolectric-r1-i6",
+    "7.1.0_r7-robolectric-r1-i6",
+    "7.0.0_r1-robolectric-r1-i6",
+    "6.0.1_r3-robolectric-r1-i6",
+    "5.1.1_r9-robolectric-r2-i6",
+    "5.0.2_r3-robolectric-r0-i6",
+    "4.4_r1-robolectric-r2-i6"
 )
 
 val downloadTasks = versions.map { version ->
