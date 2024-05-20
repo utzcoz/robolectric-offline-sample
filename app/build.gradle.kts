cb65b322030675f0791ad4113c39fb794e5b416e
utzcoz <43091780+utzcoz@users.noreply.github.com>
2024-05-20 11:08:18 +0000

    Bump Composer compiler to 1.5.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
     }
     kotlinOptions { jvmTarget = "1.8" }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.11" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 
     buildTypes {
