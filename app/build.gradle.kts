1b6913941e95ee0c4dcedc3b8bce660823882b13
utzcoz <utzcoz@outlook.com>
2024-09-16 18:41:01 +0800

    Add Kotlin compose compiler for Kotlin 2.0
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.jetbrainsComposeCompiler)
     alias(libs.plugins.spotless)
 }
 
