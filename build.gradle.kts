fcb90dcd4d7e1959c15afd92f08fc2498c23af0d
utzcoz <utzcoz@outlook.com>
2024-09-16 18:41:01 +0800

    Add Kotlin compose compiler for Kotlin 2.0
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.jetbrainsComposeCompiler) apply false
     alias(libs.plugins.spotless) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
